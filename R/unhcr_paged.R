798062c3fb2774dde5644567595c16acf2e0d649
galalH <hkotri@gmail.com>
2020-10-07 08:13:51 +0200

    pass unhcr_paged() arguments on to html_paged()

--- R/unhcr_paged.R
+++ R/unhcr_paged.R
@@ -1,45 +1,19 @@
 #' Create a paged HTML document suitable for printing with UNHCR Brand
 #'
-#' This output format is similar to \code{\link{html_paged}}.
-#' @param ...,css,template,csl,highlight,pandoc_args Arguments passed to \code{\link{html_paged}()}.
-#' This is an output format based on \code{bookdown::html_document2} (which
-#' means you can use those Markdown features added by \pkg{bookdown}). The HTML
-#' output document is split into multiple pages via a JavaScript library
-#' \pkg{paged.js}. These pages contain elements commonly seen in PDF documents,
-#' such as page numbers and running headers.
-#'
-#' When a path or an url is passed to the \code{front_cover} or \code{back_cover}
-#' argument, several CSS variables are created. They are named \code{--front-cover}
-#' and \code{--back-cover} and can be used as value for the CSS property \code{background-image}.
-#' For example, \code{background-image: var(--front-cover);}. When a vector of
-#' paths or urls is used as a value for \code{front_cover} or \code{back_cover},
-#' the CSS variables are suffixed with an index: \code{--front-cover-1},
-#' \code{--front-cover-2}, etc.
-#'
 #' @param ... Arguments passed to
-#'   \code{bookdown::\link[bookdown]{html_document2}}.
-#' @param css A character vector of CSS file paths. If a path does not contain
-#'   the \file{.css} extension, it is assumed to be a built-in CSS file. For
-#'   example, \code{default-fonts} means the file
-#'   \code{pagedown:::pkg_resource('css', 'default-fonts.css')}. To see all
-#'   built-in CSS files, run \code{pagedown:::list_css()}.
-#' @param theme The Bootstrap theme. By default, Bootstrap is not used.
-#' @param template The path to the Pandoc template to convert Markdown to HTML.
-#' @param csl The path of the Citation Style Language (CSL) file used to format
-#'   citations and references (see the \href{https://pandoc.org/MANUAL.html#citations}{Pandoc documentation}).
-#' @param front_cover,back_cover Paths or urls to image files to be used
-#'   as front or back covers. Theses images are available through CSS variables
-#'   (see Details).
+#'   \code{pagedown::\link[pagedown]{html_paged}}.
 #' @references \url{https://pagedown.rbind.io}
 #' @return An R Markdown output format.
 #' @import stats utils pagedown
 #' @export
 #'
 #'
-unhcr_paged = function(
-  ..., css = c('unhcr-fonts', 'unhcr-page', 'unhcr'), theme = NULL,
-  template = pkg_resource('html', 'paged.html'), csl = NULL,
-  front_cover = NULL, back_cover = NULL
-) {
-  pagedown::html_paged( )
+unhcr_paged = function(...) {
+  cssfile <- function(...) { 
+    system.file("resources", "css", paste0(..., ".css"), package = "unhcrpagedown")
+  }
+  
+  pagedown::html_paged(
+    css = c(cssfile('unhcr-fonts'), cssfile('unhcr-page'), cssfile('unhcr')), 
+    ...)
 }
